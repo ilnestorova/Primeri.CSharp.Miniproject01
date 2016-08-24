1e7e97de8fa80ab8a3385841cea48bbb8c1eff63
ilnestorova <iliana.nestorova@gmail.com>
2016-08-24 14:09:26 +0300

    Modil06 Video2&1 Creat new README fail and MiniProjeci011

---
+++ MP01/Calculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        // definirane na biblioteka, ne e static
+        public UserInput01.UInput uInput = new UserInput01.UInput();
+            public static void Main(string[] args)
+        {
+            //predefinirame nashata programa,zashtoto
+            //Main e ot tip console app i e static, a nashia klas Calculator ne e
+            Calculator nprogram = new Calculator();
+            nprogram.uInput.JustTesting();
+        }
+    }
+}
---
+++ MP01/UserInput01/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput01
+{
+    public class UInput
+    {
+        public UInput()
+        {
+        }
+        public void JustTesting()
+        {
+            Console.WriteLine("Starting of UserInput.Iunput.JustTesting()");
+        }
+    }
+}
